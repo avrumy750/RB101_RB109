80a877bee797c7d8890b34d6dd66742c24434d39
Abraham Stern <abes6708@gmail.com>
2024-04-16 02:11:20 +0000

    rps_2.0

--- RB101/Lesson_2/rock_paper_scissors.rb
+++ RB101/Lesson_2/rock_paper_scissors.rb
@@ -1,45 +1,88 @@
-VALID_CHOICES = ['rock', 'paper', 'scissors']
+VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
 
 def prompt(message)
   puts "=> #{message}"
 end
 
-def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
+def players_choice(user_input)
+  if user_input.start_with?('r')
+    'rock'
+  elsif user_input.start_with?('p')
+    'paper'
+  elsif user_input.start_with?('sc')
+    'scissors'
+  elsif user_input.start_with?('l')
+    'lizard'
+  elsif user_input.start_with?('sp')
+    'spock'
+  end
 end
 
-def display_result(player, computer)
-  if win?(player, computer)
-    prompt "Congrats! You won!"
+moves = { rock: ['scissors', 'lizard'],
+          paper: ['rock', 'spock'],
+          scissors: ['paper', 'lizard'],
+          lizard: ['paper', 'spock'],
+          spock: ['scissors', 'rock'] }
 
-  elsif win?(computer, player)
-    prompt "Computer won!"
+def win?(hash, first, second)
+  hash[first.to_sym].include?(second)
+end
 
+def display_result(array, player, computer)
+  if win?(array, player, computer)
+    "player"
+  elsif win?(array, computer, player)
+    "computer"
   else
     prompt "It's a tie!"
   end
 end
 
-loop do
-  choice = ''
-  loop do
-    prompt "Choose one: #{VALID_CHOICES.join(', ')}"
-    choice = gets.chomp.downcase
+player_score = 0
+computer_score = 0
 
-    if VALID_CHOICES.include?(choice)
+loop do
+  loop do
+    choice = ''
+    input = ''
+    loop do
+      prompt "Choose one: #{VALID_CHOICES.join(', ')}"
+      input = gets.chomp.downcase
+      choice = players_choice(input)
+
+      if VALID_CHOICES.include?(choice)
+        break
+      else
+        prompt "That's not a valid choice."
+      end
+    end
+
+    computer_choice = VALID_CHOICES.sample
+
+    puts "Choices: You ~ #{choice}; Computer ~ #{computer_choice}."
+
+    result = display_result(moves, choice, computer_choice)
+
+    case result
+    when "player"
+      player_score += 1
+    when "computer"
+      computer_score += 1
+    end
+
+    puts "Your score: #{player_score}. Computers score: #{computer_score}."
+
+    if player_score == 3
+      puts "Congrats! You won!"
       break
-    else
-      prompt "That's not a valid choice."
+    elsif computer_score == 3
+      puts "Congrats! Looser of the year!"
+      break
     end
   end
 
-  computer_choice = VALID_CHOICES.sample
-
-  puts "You chose: #{choice.upcase}; Computer chose: #{computer_choice.upcase}."
-
-  display_result(choice, computer_choice)
+  player_score = 0
+  computer_score = 0
 
   prompt "Would you like to play again?"
   answer = gets.chomp
